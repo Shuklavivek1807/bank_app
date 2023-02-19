6e71855771b744b95f27100433bd60949e74b216
Shuklavivek1807 <shuklavivek180797@gmail.com>
2023-02-20 02:27:00 +0530

    final code

---
+++ backend/db/config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+dotenv.config({path:'./config.env'})
+const dbUrl =process.env.DATABASE;
+const PORT = process.env.PORT;
+
+const connectionParams ={
+ useNewUrlParser: true
+};
+
+mongoose
+ .connect(dbUrl, connectionParams)
+ .then(()=>{
+    console.info("Connected to the DB");
+ })
+  .catch((e)=>{
+    console.log("Error:",e);
+  })
